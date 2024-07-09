ba22ade6a9222eb4d5ca27f2fce0da68c5646d98
Badsha Karim <badshakarim10@gmail.com>
2024-07-09 02:35:35 +0100

    Updated Regions

--- 03-basics/aws-backend/main.tf
+++ 03-basics/aws-backend/main.tf
@@ -4,13 +4,13 @@ terraform {
   ## YOU WILL UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT
   ## TO SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
   #############################################################
-  # backend "s3" {
-  #   bucket         = "devops-directive-tf-state" # REPLACE WITH YOUR BUCKET NAME
-  #   key            = "03-basics/import-bootstrap/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   dynamodb_table = "terraform-state-locking"
-  #   encrypt        = true
-  # }
+   backend "s3" {
+     bucket         = "devops-directive-tf-state-bk" # REPLACE WITH YOUR BUCKET NAME
+     key            = "03-basics/import-bootstrap/terraform.tfstate"
+     region         = "us-east-1"
+     dynamodb_table = "terraform-state-locking"
+     encrypt        = true
+   }
 
   required_providers {
     aws = {
@@ -25,9 +25,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "devops-directive-tf-state" # REPLACE WITH YOUR BUCKET NAME
+  bucket        = "devops-directive-tf-state-bk" # REPLACE WITH YOUR BUCKET NAME
   force_destroy = true
 }
+
 
 resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
